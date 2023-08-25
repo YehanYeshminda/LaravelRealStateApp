a1d2ca4558d496ce861ca6d93acc91bfa0ed8269
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-08-25 12:29:27 +0400

    added admin login

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,13 @@ require __DIR__.'/auth.php';
 // auth is used to check if the user is logged in or not and if the role is admin
 Route::middleware(['auth', 'role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
 // AGENT ROUTES
 Route::middleware(['auth', 'role:agent'])->group(function() {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
+
+// ADNUB LOGIN
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
